e21b3fa912243ae014bc5b02662820869cfa48f1
Reza Hajianpour <hajianpour.mr@gmail.com>
2023-03-03 20:54:30 +0330

    Fix pathing bugs

--- src/lib/config.js
+++ src/lib/config.js
@@ -24,7 +24,7 @@ import words_zh_CN from "./lang/zh-CN/words_zh-CN.json";
 import words_zh_TW from "./lang/zh-TW/words_zh-TW.json";
 import words_en from "./lang/en/words_en.json";
 import words_fr from "./lang/fr/words_fr.json";
-import words_fa_IR from "./lang/fr/words_fa.json";
+import words_fa_IR from "./lang/fa-IR/words_fa.json";
 import words_pt_BR from "./lang/pt-BR/words_pt-BR.json";
 import words_ru from "./lang/ru/words_ru.json";
 import words_de from "./lang/de/words_de.json";
@@ -78,6 +78,6 @@ export const CONFIG = {
     readmodel: true,
     htmlcode: true,
     help: true,
-    preview: true,
-  },
+    preview: true
+  }
 };
